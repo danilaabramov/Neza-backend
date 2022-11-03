d6b81e9340fadec86275e26cd7218bc82679ad35
Danila Abramov <67269761+danilaabramov@users.noreply.github.com>
2022-11-04 02:21:37 +0400

    Delete controllers directory

--- controllers/index.js
+++
@@ -1,4 +0,0 @@
-export * as UserController from './UserController.js'
-export * as PostController from './PostController.js'
-export * as ImageController from './ImageController.js'
-export * as StocksController from './StocksController.js'
