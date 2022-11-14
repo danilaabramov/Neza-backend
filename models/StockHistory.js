27c003926e689cbf44fae0a46b8d01e8accf784f
Danila Abramov <67269761+danilaabramov@users.noreply.github.com>
2022-11-14 10:23:25 +0400

    Add files via upload

---
+++ models/StockHistory.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose"
+
+const StockHistorySchema = new mongoose.Schema({
+    shortName: {
+        type: String,
+        required: true,
+    },
+    boardId:{
+        type: String,
+        required: true,
+    },
+    imageUrl: String,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    totalCost: {
+        type: Number,
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+    },
+    currency: {
+        type: String,
+        required: true,
+    }
+}, {
+    timestamps: true,
+})
+
+export default mongoose.model('StockHistory', StockHistorySchema)
